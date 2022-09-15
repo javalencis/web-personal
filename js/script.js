1132888b057119a037fa7f05ac9cd65fc3dd13fb
Jonathan Valencia <javalencis@gmail.com>
2022-09-14 20:25:59 -0500

    header y primera section lista

---
+++ js/script.js
@@ -0,0 +1,21 @@
+
+const menu = document.querySelector('.menu-burger');
+const nav_menu = document.querySelector('.nav-menu');
+
+
+
+menu.addEventListener('click',animateBars);
+
+
+let linea1 =  document.querySelector('.line1__menu');
+let linea2 =  document.querySelector('.line2__menu');
+let linea3 =  document.querySelector('.line3__menu');
+
+
+function animateBars(){
+    linea1.classList.toggle('line1__menuActivate');
+    linea2.classList.toggle('line2__menuActivate');
+    linea3.classList.toggle('line3__menuActivate');
+    nav_menu.classList.toggle('right');
+}
+
